c217085cb868d556dace8253ab4fcbfb200f6413
Kyle Cuss <cussprogramming@gmail.com>
2022-09-24 17:25:11 -0230

    hero section completed

---
+++ src/components/HeroImg.js
@@ -0,0 +1,25 @@
+import './HeroImg.css';
+import React from 'react';
+import desk from '../assets/desk.jpg';
+import {Link} from 'react-router-dom';
+
+const HeroImg = () => {
+  return (
+    <div className='hero'>
+        <div className='mask'>
+            <img className='intro-img' src={desk} alt="Working at desk."/>
+        </div>
+        <div className='content'>
+            <p>HI, I'M KYLE CUSS</p>
+            <h1>Web Developer</h1>
+            <div>
+                <Link to='/project' className='btn'>Projects</Link>
+                <Link to='/contact' className='btn btn-light'>Contact</Link>
+            </div>
+        </div>
+    </div>
+    
+  )
+};
+
+export default HeroImg;
