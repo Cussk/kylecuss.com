9694077a45ec9e3ac2a1267d96294da0c0ea46fd
Kyle Cuss <cussprogramming@gmail.com>
2022-09-25 08:56:34 -0230

    Created dynamic hero section for alt pages

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
+import HeroImg2 from '../components/HeroImg2';
 
 const Contact = () => {
   return (
     <div>
       <NavBar/>
+      <HeroImg2 heading='CONTACT' text='Let&#39;s have a chat'/>
       <Footer/>
     </div>
   )
