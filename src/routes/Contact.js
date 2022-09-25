468f05fc2abe12c54579b3658e07584d63e8854b
Kyle Cuss <cussprogramming@gmail.com>
2022-09-25 14:44:39 -0230

    Contact page created, form designed

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -2,13 +2,15 @@ import React from 'react'
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import HeroImg2 from '../components/HeroImg2';
+import Form from '../components/Form';
 
 const Contact = () => {
   return (
     <div>
-      <NavBar/>
-      <HeroImg2 heading='CONTACT' text='Let&#39;s have a chat'/>
-      <Footer/>
+      <NavBar />
+      <HeroImg2 heading='CONTACT' text='Let&#39;s have a chat' />
+      <Form ></Form>
+      <Footer />
     </div>
   )
 }
