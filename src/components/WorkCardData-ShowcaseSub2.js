aa912ea30d929dd1ec376bea4acc9c0c44f7f147
Kyle Cuss <cussprogramming@gmail.com>
2025-09-25 10:58:15 -0230

    update resume text

--- src/components/WorkCardData-ShowcaseSub2.js
+++ src/components/WorkCardData-ShowcaseSub2.js
@@ -8,13 +8,13 @@ const ProjectCardDataShowcaseSub2 = [
     {
         vidsrc: pro1,
         title: 'Combat System',
-        text: "Combat system created with Blueprints. Equipment component to bind new weapons to player on pickup. Collision component to handle sphere straces to determine attack hits and apply damage. Attributes Component to handle stats such as health, stamina, Fury. Animations and anim notifies call for collisions to be activated a designated points, continue combos, and reset attacking state. Gameplay Tags and Enums to determine character states with State Manager Component. Targetng compenent to set/lose focus targets.",
+        text: "Combat system created with Blueprints. Equipment component to bind new weapons to the Player on pickup. Collision component to handle sphere traces to determine attack hits and apply damage. Attributes Component to handle stats such as health, stamina, and Fury. Animations and Anim Notifies call for collisions to be activated at designated points, continue combos, and reset the attacking state. Gameplay Tags and Enums to determine character states with the State Manager Component. Targeting component to set/lose focus targets.",
     },
 
     {
         vidsrc: pro2,
         title: 'AI',
-        text: "AI created with combination of Blueprints, Behaviour Trees, and Blackboards. Blueprint tasks and services applied in behaviour tree. Behaviour Tree and Blackboard used to transition states roaming, patrolling, attacking. Use of animation montages and anim notifies to determine damage, effects, sounds. Different attackcs based on range and rare attacks with percent chance to activate. Also, Enemies with entire logic in Blueprints controlling states and attack decisions.",
+        text: "AI was created with a combination of Blueprints, Behaviour Trees, and Blackboards. Blueprint tasks and services are applied in the behaviour tree. Behaviour Tree and Blackboard are used to transition between states, including roaming, patrolling, and attacking. Use of animation montages and Anim Notifies to determine damage, effects, and sounds. Different attacks based on range, as well as rare attacks with a percentage chance to activate. Also, Enemies with entire logic in Blueprints controlling states and attack decisions.",
     },
 ];
 
